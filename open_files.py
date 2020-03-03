c36bc6be133a8ff51bfeddb4dfe6e7d68dcd5521
EzequielMussambe <chipilicafamily@gmail.com>
2020-03-02 19:05:21 -0500

    Update open_files.py

--- open_files.py
+++ open_files.py
@@ -1,1 +1,11 @@
-import geopandas as gp
+import folium 
+# from folium import plugins
+# import ipywidgets
+# import geopy
+# import numpy as np
+# import pandas as pd
+# from vega_datasets import data as vds
+
+
+
+folium.Map()
